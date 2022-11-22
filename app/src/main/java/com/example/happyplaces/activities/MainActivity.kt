95c32e730d91ef11b9b2a25d6adc4765625e1a6c
sunli <sunlightsvett@gmail.com>
2022-11-22 12:10:35 +0200

    Happy Places App

--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.happyplaces.activities
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.happyplaces.adapters.HappyPlacesAdapter
 import com.example.happyplaces.database.DatabaseHandler
 import com.example.happyplaces.databinding.ActivityMainBinding
 import com.example.happyplaces.models.HappyPlaceModel
@@ -18,24 +22,48 @@ class MainActivity : AppCompatActivity() {
 
         binding?.fabAddHappyPlace?.setOnClickListener {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
-            startActivity(intent)
+            startActivityForResult(intent,ADD_PLACE_ACTIVITY_REQUEST_CODE)
         }
         getHappyPlacesListFromLocalDB()
+    }
+
+    private fun setupHappyPlacesRecyclerView(happyPlaceList: ArrayList<HappyPlaceModel>){
+        val placesAdapter = HappyPlacesAdapter(happyPlaceList)
+        binding?.rvHappyPlacesList?.layoutManager = LinearLayoutManager(this)
+        binding?.rvHappyPlacesList?.setHasFixedSize(true)
+        binding?.rvHappyPlacesList?.adapter = placesAdapter
     }
 
     private fun getHappyPlacesListFromLocalDB(){
         val dbHandler = DatabaseHandler(this)
         val getHappyPlaceList : ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList() as ArrayList<HappyPlaceModel>
         if(getHappyPlaceList.size > 0){
-            for(i in getHappyPlaceList){
-                Log.e("Title",i.title)
-                Log.e("Description",i.description)
-            }
+            binding?.rvHappyPlacesList?.visibility = View.VISIBLE
+            binding?.tvNoRecordsAvailable?.visibility = View.GONE
+            setupHappyPlacesRecyclerView(getHappyPlaceList)
+        }else{
+            binding?.rvHappyPlacesList?.visibility = View.GONE
+            binding?.tvNoRecordsAvailable?.visibility = View.VISIBLE
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         binding = null
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int,data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == ADD_PLACE_ACTIVITY_REQUEST_CODE){
+            if(resultCode == Activity.RESULT_OK){
+                getHappyPlacesListFromLocalDB()
+            }else{
+                Log.e("Activity","Cancelled or Back pressed")
+            }
+        }
+    }
+
+    companion object{
+        var ADD_PLACE_ACTIVITY_REQUEST_CODE = 1
     }
 }
--- app/src/main/java/com/example/happyplaces/adapters/HappyPlacesAdapter.kt
+++ app/src/main/java/com/example/happyplaces/adapters/HappyPlacesAdapter.kt
@@ -1,4 +1,39 @@
 package com.example.happyplaces.adapters
 
-class HappyPlacesAdapter {
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.happyplaces.models.HappyPlaceModel
+import com.example.happyplaces.databinding.ItemHappyPlaceBinding
+
+class HappyPlacesAdapter(private var list: ArrayList<HappyPlaceModel>) : RecyclerView.Adapter<HappyPlacesAdapter.ViewHolder>() {
+
+    class ViewHolder(binding: ItemHappyPlaceBinding) : RecyclerView.ViewHolder(binding.root){
+        val imageView = binding.ivPlaceImage
+        val title = binding.tvTitle
+        val description = binding.tvDescription
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(ItemHappyPlaceBinding.inflate(
+                LayoutInflater.from(parent.context) ,parent, false
+            ))
+    }
+
+    override fun onBindViewHolder(holder:ViewHolder, position: Int) {
+
+        val model = list[position]
+        if (holder is ViewHolder) {
+            holder.imageView.setImageURI(Uri.parse(model.image))
+            holder.title.text = model.title
+            holder.description.text = model.description
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
 }
